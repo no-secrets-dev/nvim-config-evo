d47eadc4dc67f03ab545e7c3a4901c8aea06e6b5
no-secrets-dev <nosecrets1123@gmail.com>
2024-12-14 13:59:53 -0500

    update terminal shortcut

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,13 +7,17 @@ return {
       size = 15,
     })
     
-    -- Explicit mappings for both modes
-    vim.keymap.set({'n', 't'}, '<C-/>', 
-      function() 
+    -- Try different key representations
+    local opts = {silent = true, noremap = true}
+        --
+    -- Method 1: Raw keycode
+    vim.keymap.set({'n', 'i', 't'}, '<C-_>', '<cmd>ToggleTerm<CR>', opts)
+        --
+    -- Debug print to check key reception
+    vim.keymap.set('n', '<C-/>', function()
+        print("Ctrl+/ pressed!")
         vim.cmd('ToggleTerm')
-      end, 
-      {silent = true, noremap = true}
-    )
+    end, {silent = false})
   end
 }
 
